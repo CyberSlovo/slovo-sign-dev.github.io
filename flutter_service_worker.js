61833d52e202ffb882295da69e9ebcf3885bd061
Rakhimyan <rahimtoy@gmail.com>
2022-08-04 05:44:22 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "f334154b432376797ab2eaeabec85dfc",
 "index.html": "1905d24fdf273d6edae8ad9396c027a5",
 "/": "1905d24fdf273d6edae8ad9396c027a5",
-"main.dart.js": "a6740c8fc078895c0f8eae20b79377a4",
+"main.dart.js": "221592e3c93afcd0557cdbd75854b822",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
